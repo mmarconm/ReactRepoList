85d27a7cd0228b66e1263ebaf67f3efb00b7ee33
Marcelo Marcon <marconm.inf0@gmail.com>
2019-01-04 22:51:38 -0300

    final

--- src/components/github/ReposContainer.js
+++ src/components/github/ReposContainer.js
@@ -1,23 +1,58 @@
 import React, { Component } from "react";
 //
 import { fetchRepos } from "../../services/repos-api";
+import ReposList from "./ReposList";
 
 class ReposContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      repos: []
+      repos: [],
+      username: ""
     };
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  componentDidMount() {
+    fetchRepos("mmarconm").then(res => this.setState({ repos: res.data }));
   }
 
   //
-  componentDidMount() {
-    fetchRepos("mmarconm").then(res => window.console.log(res.data));
+  changeHandler(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+    fetchRepos(this.state.username).then(res =>
+      this.setState({ repos: res.data })
+    );
   }
 
   //
   render() {
-    return <h1>Repos Container</h1>;
+    return (
+      <div className="container">
+        <br />
+        <div className="jumbotron">
+          <h3>Github Repo Search</h3>
+          <p>Pesquise por repositorios de outros colaboradores é Facil :D</p>
+        </div>
+        <div className="form-group">
+          <form className="" onSubmit={this.submitHandler}>
+            <input
+              onChange={this.changeHandler}
+              className="form-control"
+              type="search"
+              placeholder="Informe Usuario e Tecle enter... Ex: mmarconm"
+            />
+          </form>
+        </div>
+
+        <ReposList repos={this.state.repos} />
+      </div>
+    );
   }
 }
 
